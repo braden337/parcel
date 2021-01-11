a27dfc6e2472dbc92cc23fee5b9a9cccb4df9b65
Braden Jonsson <braden337@gmail.com>
2021-01-11 11:39:27 -0600

    Add React setup

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hello from './components/Hello';
+
+function App() {
+  return (
+    <div>
+      <Hello name="world" />
+    </div>
+  );
+}
+
+export default App;
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 // uncomment this to use async/await or generators
 // import 'regenerator-runtime/runtime';
 
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './App';
+
+ReactDom.render(<App />, app);
+
 console.log('It works');
